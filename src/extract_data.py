4ac6f4d610b8b5a6d483e8cf02e0c62bfa498fa6
Alaa Hassan <ahlaalaahmd@gmail.com>
2022-03-09 20:13:46 +0000

    extract the data

--- src/extract_data.py
+++ src/extract_data.py
@@ -28,21 +28,21 @@ def connect_to_db():
 
 
 
-def extract_data():
+def extract_data(filename):
     data_list = []
 
     try:
-        with open('chesterfield_25-08-2021_09-00-00.csv', 'r', newline='') as file:
+        with open(filename, 'r', newline='') as file:
 
             source_file = csv.reader(file)
 
             for row in source_file:
                 row_dict = {
                     'date_time':row[0],
-                    'branch_location': row[1],
+                    'branch_name': row[1],
                     'customer_name': row[2],
                     'basket_items': row[3],
-                    'paying_amount': float(row[4]),
+                    'total_amount': float(row[4]),
                     'payment_method': row[5],
                     'card_number': row[6]
                 }
@@ -70,20 +70,20 @@ def clean_data(raw_data_list):
         clean_time = f'{time}:00'
         day , month , year = date.split('/')
         clean_date = f'{year}-{month}-{day}'
-        transaction_id = '{}-{}-{}-{}'.format(date, time, raw_data.get('branch_location'),order_number )
+        transaction_id = '{}-{}-{}-{}'.format(date, clean_time, raw_data.get('branch_name'),order_number )
         
         clean_data_item= {
             'transaction_id':transaction_id,
-             'date':clean_date,
+            'date':clean_date,
             'time' : clean_time,
-             'branch_location': raw_data['branch_location'],
-             'customer_name': raw_data['customer_name'],
-              'basket_items': raw_data['basket_items'],
-              'paying_amount':raw_data['paying_amount'],
-               'payment_method': raw_data['payment_method'],
-              'card_number': raw_data['card_number']
+            'branch_name': raw_data['branch_name'],
+            'customer_name': raw_data['customer_name'],
+            'basket_items': raw_data['basket_items'],
+            'total_amount':raw_data['total_amount'],
+            'payment_method': raw_data['payment_method'],
+            'card_number': raw_data['card_number']
         }
-   #     print(clean_data_item)
+        #print(clean_data_item)
         clean_data_list.append(clean_data_item)
         order_number += 1
 
@@ -95,55 +95,69 @@ def clean_data(raw_data_list):
 
 def get_baskets_list(clean_list):
 
+
     baskets_list = []
 
     for data in clean_list:
 
-        basket= (data['basket_items']).split(',')
+        orders = (data['basket_items']).strip('"').split(',')
+        i = 0
+        for order in orders:
 
-        for items in basket:
-            item_dict = {}
-            items_list= items.split('-')
+            items_list= order.split(' - ')
 
-            if (str(items_list[0]).strip()).startswith('Regular'):
-                if 'Flavoured' in str(items_list[0]):
+            item_dict = {'transaction_id': data.get('transaction_id'),
+                        'item_name':'',
+                        'item_flavour':'Standard',
+                        'item_size':'',
+                        'item_price': float(items_list[-1]) }
+
+            if items_list[0].strip().startswith('Regular'):
+                if 'Flavoured' in items_list[0]:
                         
                     item_dict['item_size'] ='Regular'
                     item_dict['item_flavour'] = items_list[1]
-                    item_dict['item_name'] = (str(items_list[0])[18:])
-
+                    item_dict['item_name'] = (items_list[0].split(' Flavoured '))[1]
 
                 else:
                     item_dict['item_size'] ='Regular'
-                    item_dict['item_name'] = str(items_list[0])[7:]
+                    item_dict['item_name'] = str(items_list[0])[8:]
 
-            elif (str(items_list[0]).strip()).startswith('Large'):
-                if 'Flavoured' in str(items_list[0]):
+            elif items_list[0].strip().startswith('Large'):
+                if 'Flavoured' in items_list[0]:
 
                     item_dict['item_size'] ='Large'
                     item_dict['item_flavour'] = items_list[1]
-                    item_dict['item_name'] = str(items_list[0])[16:]
-
-
+                    item_dict['item_name'] = (str(items_list[0]).split(' Flavoured '))[1]
                 else:
-
                     item_dict['item_size'] ='Large'
-                    item_dict['item_name'] = str(items_list[0])[6:]
+                    item_dict['item_name'] = items_list[0][6:]
 
-
-                item_dict ['item_price'] = float(items_list[-1])
-                item_dict['transaction_id']= data.get('transaction_id')
-                baskets_list.append(item_dict)
- 
-   # print(baskets_list)
+            baskets_list.append(item_dict)
 
     return baskets_list
 
+    
 
 
-list_data = extract_data()
-clean_data(list_data)
+list_data = extract_data('chesterfield_25-08-2021_09-00-00.csv')
+clean_data_list = clean_data(list_data)
+baskets =get_baskets_list(clean_data_list)
 
+
+for basket in baskets:
+    print(basket)
+
+
+# connection = connect_to_db()
+# connection.autocommit = True
+# cursor = connection.cursor()
+
+# query =  '''Drop TABLE employees; '''
+
+# cursor.execute(query)
+# connection.commit()
+# connection.close()
 
 
 
